5df4e3a7786efbddab63505adaded0ee9765c9f8
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-02-19 16:19:11 +0100

    Finish education and experience rendering restructure

--- src/components/ResumeExperience.jsx
+++ src/components/ResumeExperience.jsx
@@ -1,12 +1,14 @@
 export default function ResumeExperience({ response }) {
+  if (!response) return null;
   const workResponse = response;
-  return (
-    <div className="work-display">
-      <h1>{workResponse[0].workName}</h1>
-      <h1>{workResponse[0].workTitle}</h1>
-      <h1>{workResponse[0].workResponsibilities}</h1>
-      <h1>{workResponse[0].workDateStart}</h1>
-      <h1>{workResponse[0].workDateEnd}</h1>
+  const parsedResponses = workResponse.map((entry, index) => (
+    <div className="work-entry" key={index}>
+      <h1>{entry.workName}</h1>
+      <h1>{entry.workTitle}</h1>
+      <h1>{entry.workResponsibilities}</h1>
+      <h1>{entry.workDateStart}</h1>
+      <h1>{entry.workDateEnd}</h1>
     </div>
-  );
+  ));
+  return <div className="work-display">{parsedResponses}</div>;
 }
